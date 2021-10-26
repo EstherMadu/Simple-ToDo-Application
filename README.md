01e31f9825f0773ad7b6eae488096a7611a99248
EstherMadu <prityexsta@gmail.com>
2021-10-26 12:24:40 -0400

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Simple-ToDo-Application
---
+++ index.js
@@ -0,0 +1,35 @@
+const tasks = [
+    {
+    "id": "1",
+    "content": "Build out Analytics Engine",
+    "pending": true
+    },
+    
+    {
+    "id": "2",
+    "content": "Work on Carts and checkout",
+    "pending": true},
+    {
+
+    "id": "3",
+    "content": "Call with Dotun",
+    "pending": true},
+
+    {
+    "id": "4",
+    "content": "Call with Maro",
+    "pending": true},
+
+    {
+    "id": "5",
+    "content": "Wrap up exohub",
+    "pending": true},
+    
+    
+    {
+    "id": "6",
+    "content": "Update the pickup rate code for lagos to lagos shipments",
+    "pending": true},
+    
+    
+    ]
